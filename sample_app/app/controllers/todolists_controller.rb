897a46c8a5267a3f73725e69137ced813ae9ed82
tetsuji-kawatsuji <tetsuji.kawatsuji@gmail.com>
2020-06-14 21:09:40 +0900

    [Finish] 3-6

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -10,8 +10,17 @@ class TodolistsController < ApplicationController
       # DBへ保存する
        list.save
       # トップ画面へリダイレクト
-       redirect_to '/top'
+       redirect_to todolist_path(list.id) #詳細画面へリダイレクト
   end
+
+  def index
+      @lists = List.all
+  end
+
+  def show
+      @list = List.find(params[:id])
+  end
+
   private
 
   def list_params
