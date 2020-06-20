bc66d18aafdaea792c4c78daffb5fdd17d07d1a2
tetsuji-kawatsuji <tetsuji.kawatsuji@gmail.com>
2020-06-20 14:02:36 +0900

    [Finish] 5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0dd8f0710c1fea3b1ce71ebfcdffcb34df9fc139fdc299ceff2f63f92a90b796b6b324da48e06bfbc0f90a4489f980c32a240236de3e98b2bb5f8995486a7f8a'
